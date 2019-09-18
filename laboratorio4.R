a404bf3896fa5f069b1274665deb99fbf7a570f2
Mayra <sil17276@uvg.edu.gt>
2019-09-18 12:33:07 -0600

    analisis de normalidad

--- laboratorio4.R
+++ laboratorio4.R
@@ -8,6 +8,32 @@
 ##    Ivan Maldonado                  ##
 ########################################
 
+
+# Importando librerÌas
+
+library(fitdistrplus)
+require(ggpubr) # Para mejorar la visualizaci√≥n gr√°fica
+require(tidyverse) # Para explotar, manipular y visualizar datos que comparten info
+require(corrplot) # Para visualizar la matriz de correlaci√≥n
+require(cluster) #Para calcular la silueta
+library(fpc) # Para hacer el plotcluster
+library(NbClust) # Para determinar el numero de clusters optimo
+library(factoextra) # Para hacer graficos bonitos de clustering
+library(rela) # Para poder utilizar paf()
+library(psych) # Para poder utilizar KMO()
+library(FactoMineR)
+library(corrplot)
+library(REdaS)
+library(ggplot2) # Graficas bonitas
+library(ggpubr) # Graficas bonitas x2
+#library(ggmap)
+library(arules) # Reglas de asociacion
+library(factoextra) 
+library(arulesViz)
+library(dplyr)
+library(caret) # Muestreo estratificado
+library(class) # Para KNN
+
 setwd("C:/Users/smayr/Documents/Tercer aÒo/Semestre 6/Data Science/Laboratorio 4/Laboratorio-4")
 
 # Leyendo el dataset de csv importacion
@@ -20,3 +46,12 @@ class(dataTrain)
 dataTest <- read.csv("test.csv", TRUE, ",")
 # Volviendo el csv en un data frame
 class(dataTest)
+
+
+
+
+#---------------------------- An·lisis Exploratorio ------------------------------------
+
+
+#Ajuste de normalidad, para la variable diesel. 
+descdist(dataTrain$diagnosis, discrete= FALSE)
